668f26fbd1d693ffb30fcbb4332f276423f1d2c2
Hunter Luisi <hluisi@gmail.com>
2019-10-09 11:47:09 -0700

    fixed random effect mixer

--- penrose_simulator/Assets/effects/RandomEffectsMixer.cs
+++ penrose_simulator/Assets/effects/RandomEffectsMixer.cs
@@ -6,6 +6,7 @@ public class RandomEffectsMixer : EffectBase {
 
   private EffectBase[] effects;
   private int total;
+  private float percent;
 
   public override void Draw() {
 
@@ -17,13 +18,19 @@ public class RandomEffectsMixer : EffectBase {
 
       float r = 0f, g = 0f, b = 0f;
       for(int j = 0; j < total; j++) {
-        r += effects[j].buffer[i].r;
-        g += effects[j].buffer[i].g;
-        b += effects[j].buffer[i].b;
+        r += effects[j].buffer[i].r * percent;
+        g += effects[j].buffer[i].g * percent;
+        b += effects[j].buffer[i].b * percent;
       }
 
       buffer[i] = new Color(r , g , b , 1f);
     }
+  }
+
+  private EffectBase GetRandomEffect() {
+    var effect = EffectFactory.CreateEffect(EffectFactory.EffectTypes[Random.Range(0,EffectFactory.EffectCount)]);
+    if(effect.Name == Name) effect = GetRandomEffect();
+    return effect;
   }
 
   public override void LoadSettings() {
@@ -32,13 +39,15 @@ public class RandomEffectsMixer : EffectBase {
 
     var debugText = string.Empty;
     for(var i = 0; i < total; i++) {
-      effects[i] = EffectFactory.CreateEffect(EffectFactory.EffectTypes[Random.Range(0,EffectFactory.EffectCount)]);
+      effects[i] = GetRandomEffect();
       effects[i].Init();
       effects[i].LoadSettings();
       debugText += (i < total - 1) ? $"{effects[i].Name}, " : $"{effects[i].Name}";
     }
 
     controller.debugText.text = debugText;
+    percent = (1f / total) * 2f;
+
   }
 
   
