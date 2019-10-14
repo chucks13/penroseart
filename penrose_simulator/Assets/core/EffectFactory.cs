d81f960edd6345d069288a471e4c48d30581c041
Hunter Luisi <hluisi@gmail.com>
2019-10-13 23:51:47 -0700

    effect names

--- penrose_simulator/Assets/core/EffectFactory.cs
+++ penrose_simulator/Assets/core/EffectFactory.cs
@@ -12,20 +12,16 @@ public static class EffectFactory {
 
   public static string[] EffectNames => GetEffectNames();
 
-  private static string[] GetEffectNames() {
-    return effectNames ?? (effectNames = EffectTypes.Select(t => t.ToString()).ToArray());
-  }
+  private static string[] GetEffectNames() { return effectNames ?? (effectNames = EffectTypes.Select(t => t.ToString()).ToArray()); }
 
   private static Type[] GetEffectTypes() {
-    return effectTypes ?? (effectTypes = Assembly.GetAssembly(typeof(EffectBase)).GetTypes()
-                                                 .Where(
-                                                   myType => myType.IsClass && !myType.IsAbstract &&
-                                                             myType.IsSubclassOf(typeof(EffectBase)) && !myType.IsSubclassOf(typeof(Transition))
-                                                 ).ToArray());
+    return effectTypes ?? (effectTypes = Assembly.GetAssembly(typeof(EffectBase)).GetTypes().Where(
+             myType => myType.IsClass && !myType.IsAbstract &&
+                       myType.IsSubclassOf(typeof(EffectBase)) &&
+                       !myType.IsSubclassOf(typeof(Transition))
+           ).ToArray());
   }
 
-  public static EffectBase CreateEffect(Type t) {
-    return Activator.CreateInstance(t) as EffectBase;
-  }
+  public static EffectBase CreateEffect(Type t) { return Activator.CreateInstance(t) as EffectBase; }
 
 }
