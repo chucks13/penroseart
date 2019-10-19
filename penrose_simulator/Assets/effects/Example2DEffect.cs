a99d326d2ab730fbfab2ab52ed3cd41f9c0d277e
Hunter Luisi <hluisi@gmail.com>
2019-10-18 17:37:39 -0700

    created twodee effect base class

--- penrose_simulator/Assets/effects/Example2DEffect.cs
+++ penrose_simulator/Assets/effects/Example2DEffect.cs
@@ -2,137 +2,28 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Example2DEffect : EffectBase {
-
-  private const int count = 3;
-  private const int width = 44;
-  private const int height = 22;
-
-  private struct Neighbor {
-
-    public int x;
-    public int y;
-    public float distance;
-    public float weight;
-
-  };
+public class Example2DEffect : TwoDeeEffect {
 
   private Settings setting;
-  private Neighbor[,] neighbors;
-  private bool initialized = false;
 
-  [HideInInspector]
-  public Color[,] rectBuffer;
-
-  public void InitWeights() {
-    // find extents of tiles;
-    float maxx = -100000;
-    float maxy = -1000000;
-    float minx = 1000000;
-    float miny = 1000000;
-
-    for(int i = 0; i < buffer.Length; i++) {
-      float x           = controller.penrose.tiles[i].center.x;
-      float y           = controller.penrose.tiles[i].center.y;
-      if(x < minx) minx = x;
-      if(y < miny) miny = y;
-      if(x > maxx) maxx = x;
-      if(y > maxy) maxy = y;
-    }
-
-    // find closest neighbors
-    Neighbor[] closeNeighbors = new Neighbor[count + 1];
-    for(int i = 0; i < count + 1; i++) {
-      closeNeighbors[i] = new Neighbor();
-
-    }
-
-
-    for (int i = 0; i < buffer.Length; i++) {
-      // find the closest neighbors
-      int   used  = 0;
-      float px    = minx;
-      float sx = (maxx - minx) / (float)(width - 1);
-      for(int x = 0; x < width; x++) {
-        float py    = miny;
-        float sy = (maxy - miny) / (float)(height - 1);
-        for(int y = 0; y < height; y++) {
-          if(x == y) continue;
-          float dx = px - controller.penrose.tiles[i].center.x;
-          float dy = py - controller.penrose.tiles[i].center.y;
-          float d = Mathf.Sqrt((dx * dx) + (dy * dy));
-
-          // we keep (count+1) samples and sort the every time
-          closeNeighbors[used].distance = d;
-          closeNeighbors[used].x = x;
-          closeNeighbors[used].y = y;
-          used++;
-          if(used == (count + 1)) {
-            Array.Sort(closeNeighbors, delegate(Neighbor a, Neighbor b) { return a.distance.CompareTo(b.distance); });
-            used--; // discard the farthest
-          }
-          py += sy;
-        }
-        px += sx;
-      }
-
-      // closest have been found
-      // get the total of all distances and copies of the neighbors
-      float total                              = 0;
-      for(int j = 0; j < count - 1; j++) {
-        neighbors[i, j] = closeNeighbors[j];
-        total += closeNeighbors[j].distance;
-      }
-
-      // weight the values
-      for(int j = 0; j < count - 1; j++) {
-        neighbors[i, j].weight = closeNeighbors[j].distance / total;
-      }
-    }
-  }
-
-// resample the rectangle into the tile buffer
-  public void Resample() {
-    for(int i = 0; i < buffer.Length; i++) {
-      Color pix = Color.black;
-      for(var j = 0; j < count; j++) {
-        pix += rectBuffer[neighbors[i,j].x,neighbors[i,j].y] * neighbors[i,j].weight;
-      }
-      buffer[i] = pix;
-    }
-  }
-
+  /// <summary>
+  /// Called ever frame to update the debug UI text element 
+  /// </summary>
+  /// <returns></returns>
   public override string DebugText() { return ""; }
 
+
+  /// <summary>
+  /// Called once when effect is created
+  /// </summary>
   public override void Init() {
     base.Init();
-    setting    = new Settings();
-    rectBuffer = new Color[width, height];
-    neighbors= new Neighbor[buffer.Length,count];
-    for(int x = 0; x < buffer.Length; x++) {
-      for(int y = 0; y < count; y++) {
-          neighbors[x,y]=new Neighbor(); 
-      }
-    }
-
+    setting = new Settings();
   }
 
-  public override void OnEnd() {  }
-
-  public override void Draw() {
-    if(!initialized) {
-      InitWeights();
-      initialized = true;
-
-    }
-    for (int x = 0; x < width; x++) {
-      for(int y = 0; y < height; y++) {
-        rectBuffer[x,y] = Color.HSVToRGB((x*0.1f+y*0.1f+ controller.dance.fixedTime)%1f, 1f, 1f);
-      }
-    }
-    Resample();
-  }
-
+  /// <summary>
+  /// Called when effect is selected by controller to be drawn every frame
+  /// </summary>
   public override void OnStart() {
     if(controller.example2dEffectSettings.Length > 0) {
       setting = controller.example2dEffectSettings[Random.Range(0, controller.example2dEffectSettings.Length)];
@@ -140,15 +31,37 @@ public class Example2DEffect : EffectBase {
       setting.Randomize();
     }
 
-    controller.debugText.text = $"";
     buffer.Clear();
   }
 
-  [System.Serializable]
-  public class Settings {
-    public void Randomize() {
+  /// <summary>
+  /// Called when effect is no longer selected to be drawn by the controller
+  /// </summary>
+  public override void OnEnd() { }
+
+  /// <summary>
+  /// Called every frame by controller when the effect is selected
+  /// </summary>
+  public override void Draw() {
+    for(int x = 0; x < width; x++) {
+      for(int y = 0; y < height; y++) {
+        twoDeeBuffer[x, y] = Color.HSVToRGB((x * 0.1f + y * 0.1f + controller.dance.fixedTime) % 1f, 1f, 1f);
+      }
     }
+
+    // convert the 2D Matrix buffer to a tile buffer
+    TwoDeeEffect.ConvertBuffer(ref twoDeeBuffer, in buffer);
+  }
+
+
+  /// <summary>
+  /// put all data that can be changed or saved here
+  /// </summary>
+  [Serializable]
+  public class Settings {
+
+    public void Randomize() { }
+
   }
 
 }
-
