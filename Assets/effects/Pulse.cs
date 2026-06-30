172804eb70f9844bb4ad1238460f698dd6f6e887
chucks13 <chucks@he.net>
2026-06-30 11:14:10 -0700

    beat adaptive pulse

--- Assets/effects/Pulse.cs
+++ Assets/effects/Pulse.cs
@@ -13,17 +13,18 @@ public class Pulse : EffectBase
     private float seconds;
     private Color color;
     private float colorDelta;
-    private float[] wave = new float[100];
+    private float[] wave = new float[400];
     int beatMode;
     float maxradius = 0;
     float pulseMultipler;
+    float pulseScale;
 
     /// <summary>
     /// Returns text for the Controller debug display while this effect is active.
     /// </summary>
     public override string DebugText()
     {
-        return $"Start: {startColor}\nEnd: {endColor}\nTime: {seconds}";
+        return $"scale: {pulseScale} \nStart: {startColor}\nEnd: {endColor}\nTime: {seconds}";
     }
 
     /// <summary>
@@ -41,14 +42,17 @@ public class Pulse : EffectBase
     public override void OnStart()
     {
         base.OnStart();
+        wave = new float[400];      // clear array
         color = Color.HSVToRGB(Random.value, 1f, 1f);
         seconds = Random.Range(1f, 5f);
         colorDelta = Random.Range(0.25f, 0.75f);
-        beatVariant = beatManager.GetRandomVariantChill();
+        beatVariant = beatManager.GetRandomVariant();
         startColor = color;
         endColor = startColor.Delta(colorDelta);
         beatMode = Random.Range(0, 2);
         pulseMultipler = Random.value * 0.125f + 0.125f;
+        pulseScale = beatManager.GetShortestNonZeroPeakSpacingMs(beatVariant) / 200;
+
         for (int i = 0; i < buffer.Length; i++)
         {
             float r = tiles[i].radius;
@@ -73,14 +77,14 @@ public class Pulse : EffectBase
             wave[i] = wave[i - 1];
         wave[0] = waveHeight;
 
-
         var color1 = Color.Lerp(color, endColor, t);
         var color2 = Color.Lerp(endColor, color, t);
 
         for (int i = 0; i < buffer.Length; i++)
         {
-            int waveidx = (int)(tiles[i].radius / maxradius * (wave.Length - 1));
-            if (waveidx >= wave.Length)
+            float waveidxf = tiles[i].radius * pulseScale;
+            int waveidx = (int)waveidxf;
+            if (waveidx > (wave.Length - 1))
                 waveidx = wave.Length - 1;
 
             Color color = tiles[i].type == 0 ? color1 : color2;
